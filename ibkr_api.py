dbd0542f3699ec4f82e576de929eea9c843aa95c
travis3630 <43075484+travis3630@users.noreply.github.com>
2024-01-06 16:39:34 +0800

    [Update] api submodule to latest branch

--- ibkr_api.py
+++ ibkr_api.py
@@ -16,6 +16,7 @@ import datetime
 
 IBKR_PERIOD_MAPPING = {
     "5m"  :  "5 mins",
+    "10m" : "10 mins",
     "30m" : "30 mins",
     "1h"  :  "1 hour",
     "4h"  : "4 hours",
@@ -30,12 +31,16 @@ class IbkrApi(EWrapper, EClient):
         self.clientId = clientId
 
         # List of temporary variables
-        self.requestId = 2000
+        # self.reqId = ibkrApi.reqIds(-1) + 10000
+        # self.orderId = ibkrApi.reqIds(-1) 
+        self.reqId = 10000
+        self.orderId = 1
         self.contract = None
         self.histDataTemp = []
         self.conDetTemp = Contract()
         self.serverTime = None
-
+        self.accountDataTemp = []
+       
     def connect(self):
         return super().connect(self.host, self.port, self.clientId)
 
@@ -47,12 +52,29 @@ class IbkrApi(EWrapper, EClient):
 
     def resetHistDataTemp(self) -> None:
         self.histDataTemp = []
+    
+    def resetAccountDataTemp(self) -> None:
+        print("reset account data temp")
+        self.accountDataTemp = []
 
-    # Pending Xian Li verify this, I probably wrong with request ID
-    def updateReqId(self) -> None:
-        self.requestId += 1
-        if self.requestId > 9999:
-            self.requestId = 2000
+    def get_order_id(self):
+        '''
+        Segregation between reqId and orderId. Chances are you will only have 1M orderIds but unlimited reqIDs in your lifetime.
+        Auto orderId generator that appends +1 every time it is being called. Could use .reqIds(-1) to verify.
+        '''
+        # if ibkrApi.reqIds(-1) relook into reset
+        current_id = self.orderId
+        self.orderId = (self.orderId + 1) % 10000  # Reset to 1 if it exceeds 9999
+        return current_id
+
+    def get_req_id(self):
+        '''
+        Segregation between reqId and orderId. Chances are you will only have 1M orderIds but unlimited reqIDsin your lifetime.
+        Auto reqId generator that appends +1 every time it is being called. Could use .reqIds(-1) to verify.
+        '''
+        current_id = self.reqId
+        self.reqId = (self.reqId + 1) % 20000  # Reset to 10000 if it exceeds 19999
+        return current_id
 
     def createContract(self, symbol, secType, exchange, currency):
         self.contract = Contract()
@@ -60,38 +82,85 @@ class IbkrApi(EWrapper, EClient):
         self.contract.secType = secType
         self.contract.exchange = exchange
         self.contract.currency = currency
+        return self.contract
 
-    def isRegTradingHour(self, reqId) -> bool:
+    #! [bracket]
+    def BracketOrder(self, parentOrderId:int, action:str, quantity:float, 
+                     limitPrice:float, takeProfitLimitPrice:float, 
+                     stopLossPrice:float):
+        """ 
+        Args:   
+        parentOrderId: generated from api.get_order_id()
+        action: BUY or SELL
+        quantity: number of positions 
+        limitPrice: float
+        takeProfitLimitPrice: float
+        stopLossPrice: float
+           
+        Description: 
+        #/ Bracket orders are designed to help limit your loss and lock in a profit by "bracketing" an order with two opposite-side orders. 
+        #/ A BUY order is bracketed by a high-side sell limit order and a low-side sell stop order. A SELL order is bracketed by a high-side buy 
+        #/ stop order and a low side buy limit order.
+        #/ Products: CFD, BAG, FOP, CASH, FUT, OPT, STK, WAR
 
+        Returns: bracketOrder: list of Order(s)
+        """
+        #This will be our main or "parent" order
+        parent = Order()
+        parent.orderId = parentOrderId
+        print(1)
+        parent.action = action
+        parent.orderType = "LMT"
+        parent.totalQuantity = quantity
+        parent.lmtPrice = limitPrice
+        #The parent and children orders will need this attribute set to False to prevent accidental executions.
+        #The LAST CHILD will have it set to True, 
+        parent.transmit = False
+
+        takeProfit = Order()
+        takeProfit.orderId = parentOrderId + 1
+        takeProfit.action = "SELL" if action == "BUY" else "BUY"
+        takeProfit.orderType = "LMT"
+        takeProfit.totalQuantity = quantity
+        takeProfit.lmtPrice = takeProfitLimitPrice
+        takeProfit.parentId = parentOrderId
+        takeProfit.transmit = False
+
+        stopLoss = Order()
+        stopLoss.orderId = parentOrderId + 2
+        stopLoss.action = "SELL" if action == "BUY" else "BUY"
+        stopLoss.orderType = "STP"
+        #Stop trigger price
+        stopLoss.auxPrice = stopLossPrice
+        stopLoss.totalQuantity = quantity
+        stopLoss.parentId = parentOrderId
+        #In this case, the low side order will be the last child being sent. Therefore, it needs to set this attribute to True 
+        #to activate all its predecessors
+        stopLoss.transmit = True
+        bracketOrder = [parent, takeProfit, stopLoss]
+        return bracketOrder
+
+    def isRegTradingHour(self) -> bool:
+        reqId = self.get_req_id()
         # request for contract details
         self.reqContractDetails(reqId, self.contract)
-
         # time allow API to get data 
         systime.sleep(3)
-
         contractDetail = self.conDetTemp
-
         # Note that tradingHours format:
         # "%Y%m%d:%H%M-%Y%m%d:%H%M;%Y%m%d:%H%M-%Y%m%d:%H%M:%Y%m%d:%H%M:CLOSED" Format length last for a week
         # It is being dissected as below
-
         tradingHours = contractDetail.tradingHours
         tradingRanges = tradingHours.split(';')
-
         for tradingRange in tradingRanges:
             closedKeyword = False
 
-            if len(tradingRange) == 0: # string is empty
-                pass 
-
-            elif "CLOSED" in tradingRange:
+            if "CLOSED" in tradingRange:
                 closedKeyword = True
-
             else:
                 startTime, endTime = tradingRange.split('-')
                 startTime = datetime.datetime.strptime(startTime, "%Y%m%d:%H%M")
                 endTime = datetime.datetime.strptime(endTime, "%Y%m%d:%H%M")
-
                 # Check if current time is within this trading range
                 currTime = self.getCurrTime()
                 # Convert serverTime string to datetime format
@@ -99,7 +168,6 @@ class IbkrApi(EWrapper, EClient):
 
                 if not closedKeyword and startTime <= currTime <= endTime:
                     return 1
-
         return 0
 
     def contractDetails(self, reqId, contractDetails):
@@ -128,22 +196,20 @@ class IbkrApi(EWrapper, EClient):
         currentSysTime = systime.time()
         localTime = systime.localtime(currentSysTime)
         localOffsetSec = systime.mktime(localTime) - currentSysTime
-
         # Calculate the offset in hours
         localOffsetHr = datetime.timedelta(hours=round(localOffsetSec / 3600))
-
         #ukOffset = datetime.timedelta(hours=0)  # UK is UTC+0
         usEasternOffset = datetime.timedelta(hours=-4)  # US/Eastern is UTC-4
-
         self.serverTime = datetime.datetime.fromtimestamp(time) + (usEasternOffset - localOffsetHr)
         self.serverTime = self.serverTime.strftime("%Y%m%d %H:%M:%S US/Eastern")
 
     def getHistoricalData(self, period, duration):
 
         self.resetHistDataTemp()
-        rth = self.isRegTradingHour(1000)
-
-        self.reqHistoricalData(self.requestId, self.contract, self.serverTime, duration, IBKR_PERIOD_MAPPING[period], 'BID', rth, 1, False, [])
+        rth = self.isRegTradingHour()
+        reqId = self.get_req_id()
+        #reqId updated by XL need double check
+        self.reqHistoricalData(reqId, self.contract, self.serverTime, duration, IBKR_PERIOD_MAPPING[period], 'BID', rth, 1, False, [])
         systime.sleep(5)
 
         return self.histDataTemp
@@ -158,19 +224,76 @@ class IbkrApi(EWrapper, EClient):
             "volume": bar.volume
         })
 
-    def nextValidId(self, orderId: int):
-        super().nextValidId(orderId)
-        self.nextorderId = orderId
-        print('The next valid order id is: ', self.nextorderId)
+# Added other portfolio viewing API
+    def error(self, reqId, errorCode, errorString, errorHint):
+        print("Error: ", reqId, " ", errorCode, " ", errorString, " ", errorHint)
 
-    def orderStatus(self, orderId, status, filled, remaining, avgFullPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
-        print('orderStatus - orderid:', orderId, 'status:', status, 'filled', filled, 'remaining', remaining, 'lastFillPrice', lastFillPrice)
+    def getCashVal(self, tags:str):
+        '''
+        Portfolio viewing API: get portfolio summary only using tags as input via .reqAccountSummary with 
+        Parameters
+        ------
+        group	Default set to "All" to return account summary data for all accounts, or set to a specific Advisor Account Group name that has already been created in TWS Global Configuration.
+        tags	a comma separated list with the desired tags:
 
-    def openOrder(self, orderId, contract, order, orderState):
-        print('openOrder id:', orderId, contract.symbol, contract.secType, '@', contract.exchange, ':', order.action, order.orderType, order.totalQuantity, orderState.status)
+        Return:
+        run callback function self.accountDataTemp to get the account summary data
+        '''
+        try:
+            reqId = self.get_req_id()
+            self.resetAccountDataTemp()
+            self.reqAccountSummary(reqId, "All", tags)
+            systime.sleep(1)  
+            self.cancelAccountSummary(reqId)
+            return self.accountDataTemp
+        except Exception as e:
+            print(e)
+            return []
 
-    def execDetails(self, reqId, contract, execution):
-        print('Order Executed: ', reqId, contract.symbol, contract.secType, contract.currency, execution.execId, execution.orderId, execution.shares, execution.lastLiquidity)
+    # def updatePortfolio(self, contract: Contract, position: float, marketPrice: float, marketValue: float,
+    #                     averageCost: float, unrealizedPNL: float, realizedPNL: float, accountName: str):
+        '''
+        Portfolio viewing API
+        '''
+    #     try:
+    #         super().updatePortfolio(contract, position, marketPrice, marketValue,
+    #                                 averageCost, unrealizedPNL, realizedPNL, accountName)
+    #         print("UpdatePortfolio.", "Symbol:", contract.symbol, "SecType:", contract.secType, "Exchange:", contract.exchange,
+    #           "Position:", position, "MarketPrice:", marketPrice, "MarketValue:", marketValue, "AverageCost:", averageCost,
+    #           "UnrealizedPNL:", unrealizedPNL, "RealizedPNL:", realizedPNL, "AccountName:", accountName)
+    #     except Exception as e:
+    #         print(e)
+    #         return []
+
+    def accountSummary(self, reqId: int, account: str, tag: str, value: str, currency: str):
+        '''
+        Callback function
+        '''
+        super().accountSummary(reqId, account, tag, value, currency)
+        self.accountDataTemp.append({
+            "AccountSummary. ReqId": reqId,
+            "Tag": tag,
+            "Value": value, 
+            "Currency": currency, 
+            "AccountName": account 
+        })
+
+    def accountSummaryEnd(self, reqId: int):
+        '''
+        Callback function that terminate the .reqAccountSummarygicom request
+        '''
+        super().accountSummaryEnd(reqId)
+        print("AccountSummaryEnd.Curr ReqId:", reqId)
+        print("Next Valid ID:",reqId+1)
+
+    def updateAccountTime(self, timeStamp: str):
+        super().updateAccountTime(timeStamp)
+        print("UpdateAccountTime. Time:", timeStamp)
+
+    def accountDownloadEnd(self, accountName: str):
+        super().accountDownloadEnd(accountName)
+        print("AccountDownloadEnd. Account:", accountName)
 
     def runLoop(self):
-        self.run()
+            self.run()
+       
